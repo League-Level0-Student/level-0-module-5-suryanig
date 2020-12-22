e8fe6ddbdc0a527d95b6a6e79ccfc32562a0dcda
suryanig <67238881+suryanig@users.noreply.github.com>
2020-12-21 18:30:34 -0800

    I finished Goofy Names and Snowflake.

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,29 +11,32 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String name = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-
+		// Run your program to see that this works.
+		System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 7).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < name.length(); i++) {
 
-				// 4. Create a char variable to store the next character of the name
-				//    use .charAt()
-	
-				
-				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
-			
-				// 6. Even characters should be made uppercase and odd characters made lowercase
-				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
-	
-			
-				// 7. ADD the char to the end of the goofyName String
+			// 4. Create a char variable to store the next character of the name
+			// use .charAt()
+			char letter = name.charAt(i);
 
-		
+			// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
+			if (i%2==0){
+				letter = Character.toUpperCase(letter);
+			}
+			else {
+				letter = Character.toLowerCase(letter);
+			}
+			// 6. Even characters should be made uppercase and odd characters made lowercase
+			// HINT: use Character.toUpperCase() or Character.toLowerCase()
+
+			// 7. ADD the char to the end of the goofyName String
+			goofyName+=letter;
+		}
 		// 8. Use pop-up to show user their Goofy name
-
+		JOptionPane.showMessageDialog(null, "Your goofy name is " + goofyName);
 	}
 }
-
