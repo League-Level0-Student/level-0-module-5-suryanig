2f3ff6784fbf03c5aa4140215cbe0aeafa08cc71
suryanig <67238881+suryanig@users.noreply.github.com>
2021-01-25 18:26:39 -0800

    I finished for loop gauntlet today

--- src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGaunlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGaunlet.java
@@ -43,5 +43,30 @@ public static void main(String[] args) {
 		System.out.println(" years old.");
 		
 	}
+	for (int c = 0; c < 3; c++) {
+		for (int n = 0; n < 3; n ++) {
+		System.out.println(c +" "+ n);
+	}
+	}
+	// x = x + 3; x += 3;
+	for (int x = 1; x < 8; x = x + 3) {
+		for (int q = x; q < x+3; q++) {
+			System.out.print(q + " ");
+		}
+		System.out.println();
+	}
+	for (int y = 1; y < 92; y = y + 10) {
+		for (int z = y; z < y+10; z++) {
+			System.out.print(z + " ");
+		}
+		System.out.println();
+	}
+	for (int p = 1; p < 7; p++) {
+		for ( int n = 0; n < p; n++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+	
 }
 }
